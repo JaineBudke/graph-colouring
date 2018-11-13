d1b4a91481c5e789a10ffcde33b497bbabccd8c2
jainebudke <jainebudke@hotmail.com>
2018-11-13 09:54:51 -0300

    adiciona metodos

--- src/metaheuristic_algorithms/TabuSearch.java
+++ src/metaheuristic_algorithms/TabuSearch.java
@@ -9,7 +9,35 @@ public class TabuSearch {
 
 	//Lista que guarda as soluções
 	ArrayList <ArrayList<Vertex>> tabuList;
+	// Lista de vertices do grafo
+	ArrayList<Vertex> vertexes;
 	
+	
+	
+	public ArrayList<Vertex> initialSolution() {
+		
+		// atribui uma cor a cada vertice
+		
+		return vertexes;
+	}
+	
+	public boolean stopCriteria() {
+		
+		// para apos 10 solucoes sem diminuir numero de conflitos
+		
+		return true;
+	}
+	
+	
+	public ArrayList<ArrayList<Vertex>> getNeighbors( ArrayList<Vertex> bestCandidate ){
+		
+		// percorre n/2
+			// escolhe aleatoriamente um vertice
+			// calcula conflitos
+			// recupera solucao com menos conflitos (treemap)
+		
+		return null;
+	}
 	
 	
 	
@@ -21,13 +49,13 @@ public class TabuSearch {
 		//Inicialização 
 		//Uma solução será uma lista de vértices coloridos
 		ArrayList<Vertex> vertexes = g.cloneVertexes();
-		ArrayList<Vertex> s0 = initialSolution(vertexes);
+		ArrayList<Vertex> s0 = initialSolution();
 		ArrayList<Vertex> bestSolution = s0;
 		ArrayList<Vertex> bestCandidate = s0;
 		tabuList = new ArrayList<ArrayList<Vertex>>();
 		tabuList.add(s0);
 		
-		while( /*stop criteria*/) {
+		while( stopCriteria() ) {
 			ArrayList<ArrayList<Vertex>> neighborhood = getNeighbors(bestCandidate);
 			for( ArrayList<Vertex> candidate :  neighborhood) {
 				// Search the best solution in the neighborhood
@@ -37,6 +65,7 @@ public class TabuSearch {
 					if(fitnessCandidate > fitnessBestCandidate) {
 						//update the best candidate
 						bestCandidate = candidate;
+						
 					}
 				}
 			}
@@ -44,6 +73,7 @@ public class TabuSearch {
 			// Check if the best neighbor is the best solution so far
 			if(calculateFitness(bestCandidate) > calculateFitness(bestSolution)) {
 				bestSolution = bestCandidate;
+				// salva numero de conflitos de melhor candidato
 			}
 			
 			// Keep the best candidate at the tabu list
@@ -56,6 +86,7 @@ public class TabuSearch {
 			return bestSolution;
 			
 		}
+		return bestCandidate;
 		
 		
 	}
@@ -66,7 +97,11 @@ public class TabuSearch {
 	 * @return
 	 */
 	private float calculateFitness(ArrayList<Vertex> candidate) {
-		// TODO Auto-generated method stub
+		
+		// percorre vertices da solucao candidata
+			// percorre os adjacentes de v
+				// calcula numero de vertices adjacentes que tem mesma cor de v
+		
 		return 0;
 	}
 }
